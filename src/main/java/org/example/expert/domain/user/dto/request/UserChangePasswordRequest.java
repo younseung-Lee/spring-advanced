b45c26196c4763274c9e5cb94a765fc2b5ff3bef
gram <gram@DESKTOP-E54V6V8>
2025-04-18 13:09:15 +0900

    레벨 1-3: Validation 활용

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,10 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
     @NotBlank
+    @Size(min = 8, message = "새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.")
+    @Pattern(regexp = ".*\\d.", message = "새 비밀번호에는 숫자가 포함해야 합니다.")
+    @Pattern(regexp = ".*[A-Z].*", message = "새 비밀번호에는 대문자가 포함해야 합니다.")
     private String newPassword;
 }
